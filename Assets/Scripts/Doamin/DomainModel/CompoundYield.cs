2af9647b82dd17df434518faf98066096290e19c
nke-type0 <52362979+nke-type0@users.noreply.github.com>
2023-07-18 17:28:14 +0900

    add: CompoundYield

---
+++ Assets/Scripts/Doamin/DomainModel/CompoundYield.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class CompoundYield
+{
+    [SerializeField] float _value;
+    public float Value => _value;
+
+    private const int Min = 0;
+
+    public CompoundYield(float value = 0)
+    {
+        if (value < Min)
+        {
+            throw new ArgumentException("利回りは0以上を指定してください");
+        }
+
+        this._value = value;
+    }
+}
