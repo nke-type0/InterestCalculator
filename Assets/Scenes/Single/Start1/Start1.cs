ee72815f72be87e77284ffa2e064aa61c545cb9b
nke-type0 <52362979+nke-type0@users.noreply.github.com>
2023-07-26 08:49:10 +0900

    update: SceneSettings, SceneInBuildSettings

---
+++ Assets/Scenes/Single/Start1/Start1.cs
@@ -0,0 +1,16 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start1 : MonoBehaviour
+{
+    private async void Start()
+    {
+
+        string sceneName = new string("Main1");
+        await SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+    }
+}
