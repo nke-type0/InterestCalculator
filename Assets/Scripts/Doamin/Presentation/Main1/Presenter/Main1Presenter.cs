77521d93a5d62afb2a897f522c1a5e8e104045be
nke-type0 <52362979+nke-type0@users.noreply.github.com>
2023-07-26 08:25:21 +0900

    add: View, Presenter

---
+++ Assets/Scripts/Doamin/Presentation/Main1/Presenter/Main1Presenter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Zenject;
+
+public class Main1Presenter : MonoBehaviour
+{
+
+    [Inject] private Main1View _main1View;
+
+    private async void Start()
+    {
+
+        //仮止め実装
+        ////初期額
+        //InitalAmount initalAmount = new InitalAmount(5000000);
+        ////積立額
+        //ReserveAmount reserveAmount = new ReserveAmount(0);
+        ////積立年数
+        //AccumulationPeriod accumulationPeriod = new AccumulationPeriod(30);
+        ////利率
+        //CompoundYield compoundYield = new CompoundYield(3);
+
+        //TotalReserveAmount totalReserveAmount = new TotalReserveAmount(
+        //    initalAmount, reserveAmount, accumulationPeriod, compoundYield);
+
+        ////totalReserveAmount.PrincipalCalculation();
+        //totalReserveAmount.RatePrincipalCaluculation();
+
+
+
+
+        _main1View.CaluculateButton.OnClickAsObservable()
+                .Subscribe(_ => Debug.Log("")).AddTo(this);
+
+    }
+
+}
---
+++ Assets/Scripts/Doamin/Presentation/Main1/View/Main1View.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main1View : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _compoundYieldText;
+    public TextMeshProUGUI CompoundYieldText => _compoundYieldText;
+
+    [SerializeField] TextMeshProUGUI _initialAmountText;
+    public TextMeshProUGUI InitialAmountText => _initialAmountText;
+
+    [SerializeField] TextMeshProUGUI _reserveAmountText;
+    public TextMeshProUGUI ReserveAmountText => _reserveAmountText;
+
+    [SerializeField] TextMeshProUGUI _accumulationPeriodText;
+    public TextMeshProUGUI AccumulationPeriodText => _accumulationPeriodText;
+
+
+    [SerializeField] TMP_InputField _compoundYieldInput;
+    public TMP_InputField CompoundYieldInput => _compoundYieldInput;
+
+    [SerializeField] TMP_InputField _initialAmountInput;
+    public TMP_InputField InitialAmountInput => _initialAmountInput;
+
+    [SerializeField] TMP_InputField _reserveAmountInput;
+    public TMP_InputField ReserveAmountInput => _reserveAmountInput;
+
+    [SerializeField] TMP_InputField _accumulationPeriodInput;
+    public TMP_InputField AccumulationPeriodInput => _accumulationPeriodInput;
+
+
+    [SerializeField] Button _caluculateButton;
+    public Button CaluculateButton => _caluculateButton;
+
+}
